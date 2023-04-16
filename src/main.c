b68d400bb54c337616746f5ad3b997ffd999787a
Johannes Nielsen <flykasual@gmail.com>
2023-04-16 12:07:41 +0200

    add minimal main.c
    Signed-off-by: Johannes Nielsen <flykasual@gmail.com>

---
+++ src/main.c
@@ -0,0 +1,31 @@
+/**
+ * c_cipher – a simple cli tool for symmetric de-/encryption
+ * Copyright (C) 2023  Johannes Nielsen
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(void) {
+    printf(
+        " * c_cipher  Copyright (C) 2023  Johannes Nielsen\n"
+        " * This program comes with ABSOLUTELY NO WARRANTY.\n"
+        " * This is free software, and you are welcome to redistribute it\n"
+        " * under certain conditions.\n\n"
+        " * See <https://www.gnu.org/licenses/> for details.\n"
+    );
+    return EXIT_SUCCESS;
+}
